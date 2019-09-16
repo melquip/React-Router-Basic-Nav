bc9f4fe89323acaa65ab0bfbb67be9c5c8bba146
Melquisedeque Pereira <melqui1@live.com.pt>
2019-09-16 12:23:40 +0100

    Added Links

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import { Route } from 'react-router-dom';
 const App = () => (
   <div>
     <Navigation />
-	<Route exact path="/" />
-	<Route exact path="/about" />
-	<Route exact path="/contact" />
+	<Route exact path="/" component={Home} />
+	<Route exact path="/about" component={About} />
+	<Route exact path="/contact" component={Contact} />
   </div>
 );
 
